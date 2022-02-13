a03cd630d91ab7c77f0d24c6bc25800a39855321
plinki <geolazaria@gmail.com>
2022-02-13 03:49:03 +0200

    unchecked, calldata, errors

--- contracts/Deadfish.sol
+++ contracts/Deadfish.sol
@@ -1,5 +1,5 @@
 //SPDX-License-Identifier: Unlicense
-pragma solidity ^0.8.0;
+pragma solidity >=0.8.4;
 //          __
 //         /  ;
 //     _.--"""-..   _.
@@ -13,41 +13,47 @@ pragma solidity ^0.8.0;
 /// @dev Will possibly optimize
 contract Deadfish {
 
+    error NoInstructions();
+    error TooManyInstructions();
+
     mapping(address => int256[]) plops;
 
-    function instruct(uint256[] memory instructions) external returns (int256[] memory) {
-        require(instructions.length > 1, "No instructions");
-
+    function instruct(uint256[] calldata instructions) external returns (int256[] memory) {
+        if (instructions.length < 1) revert NoInstructions();
+        if (instructions.length > 200) revert TooManyInstructions();
         delete plops[msg.sender];
 
         int256 current_accumulator = 0;
 
-        for (uint256 i = 0; i < instructions.length; ++i) {
-            if (instructions[i] == 0x69) {
-                current_accumulator += 1;
-            }
+        unchecked {
+            for (uint256 i = 0; i < instructions.length; ++i) {
+                if (instructions[i] == 0x69) {
+                    current_accumulator += 1;
+                }
 
-            if (instructions[i] == 0x64) {
-                current_accumulator -= 1;
-            }
+                if (instructions[i] == 0x64) {
+                    current_accumulator -= 1;
+                }
 
-            if (instructions[i] == 0x73) {
-                current_accumulator = current_accumulator ** 2;
-            }
+                if (instructions[i] == 0x73) {
+                    current_accumulator = current_accumulator ** 2;
+                }
 
-            if (instructions[i] == 0x6F) {
-                plops[msg.sender].push(current_accumulator);
-            }
+                if (instructions[i] == 0x6F) {
+                    plops[msg.sender].push(current_accumulator);
+                }
 
-            if (current_accumulator == 256 || current_accumulator == -1) {
-                current_accumulator = 0;
+                if (current_accumulator == 256 || current_accumulator == -1) {
+                    current_accumulator = 0;
+                }
+
             }
         }
 
         return plops[msg.sender];
     }
 
-    function getPlops() public view returns (int256[] memory) {
+    function getPlops() external view returns (int256[] memory) {
         return plops[msg.sender];
     }
 }
